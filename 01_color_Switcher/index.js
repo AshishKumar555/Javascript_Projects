a4df3a8339b67239e213595aaafe1432b5261f44
AshishKumar555 <112402883+AshishKumar555@users.noreply.github.com>
2024-01-06 12:57:49 +0530

    without loop concept created

--- 01_color_Switcher/index.js
+++ 01_color_Switcher/index.js
@@ -13,3 +13,21 @@ buttons.forEach(function(argument){
         body.style.backgroundColor=e.target.id
     })
 })
+
+/-------------Without Loop ---------------------!
+
+// const button1=document.getElementById('grey')
+// button1.addEventListener("click",function(e)
+// {body.style.backgroundColor="grey"})
+
+// const button2=document.getElementById('white')
+// button2.addEventListener("click",function(e)
+// {body.style.backgroundColor="white"})
+
+// const button3=document.getElementById('blue')
+// button3.addEventListener("click",function(e)
+// {body.style.backgroundColor="blue"})
+
+// const button4=document.getElementById('yellow')
+// button4.addEventListener("click",function(e)
+// {body.style.backgroundColor="yellow"})
